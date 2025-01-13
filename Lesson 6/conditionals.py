48de7b6431d310494fd7eefce2c56039f4329c4f
Tuhin Poddar <tuhinpoddar114@gmail.com>
2025-01-13 18:03:30 +0530

    conditionals

---
+++ Lesson 6/conditionals.py
@@ -0,0 +1,8 @@
+a = int(input("Enter your age: "))
+if a >= 18:
+    print("You are an adult")
+
+elif(a >= 13 and a < 18):
+    print("You are a teenager")
+else:
+    print("You are not an adult")
